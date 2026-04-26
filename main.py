34a35426b014d28eec34fa06345e3af81eb14529
Ser2104 <sergio.yacolcac@gmail.com>
2026-04-26 16:02:57 -0400

    Add board creation and display

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+def create_board():
+    # Creates a 3x3 board using a list of lists
+    return [[" " for _ in range(3)] for _ in range(3)]
+
+
+def print_board(board):
+    # Displays the board in a clean format
+    print()
+    for row in range(3):
+        print(" " + board[row][0] + " | " + board[row][1] + " | " + board[row][2])
+        if row < 2:
+            print("---+---+---")
+    print()
+
+
 def main():
-    print("Starting project")
+    print("Welcome to Tic-Tac-Toe!")
+
+    board = create_board()
+    print_board(board)
+
 
 if __name__ == "__main__":
     main()
